f5b7555a05558dca851c6e9737e7b3d95a028947
Vladimir <azuresplash@gmail.com>
2021-11-30 10:40:32 +0100

    Added frontend deploy action

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Col, Row } from "antd";
 import { SearchContainer } from "./SearchContainer";
 
 function App() {
+  //
   return (
     <Row justify="center">
       <Col span={24} xs={24} md={12} lg={8}>
