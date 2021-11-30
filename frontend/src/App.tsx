a89d03fa8bf37d22b0b4911b09d5f0a6734e4bcd
Vladimir <azuresplash@gmail.com>
2021-11-30 10:59:30 +0100

    Dummy change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Col, Row } from "antd";
 import { SearchContainer } from "./SearchContainer";
 
 function App() {
-  //
   return (
     <Row justify="center">
       <Col span={24} xs={24} md={12} lg={8}>
